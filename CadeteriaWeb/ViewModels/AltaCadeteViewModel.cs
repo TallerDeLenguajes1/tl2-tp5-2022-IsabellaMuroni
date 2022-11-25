16d66b8c4e8d6eea2155d74651a59eec417be0b5
IsabellaMuroni <isa_sk_@hotmail.com>
2022-11-25 11:18:35 -0300

    Se realizan cambios en la vista cadetes

--- CadeteriaWeb/ViewModels/AltaCadeteViewModel.cs
+++ CadeteriaWeb/ViewModels/AltaCadeteViewModel.cs
@@ -12,14 +12,5 @@ namespace CadeteriaWeb.ViewModels
 
         [Required][Phone]
         public int Telefono { get; set; }
-
-        public AltaCadeteViewModel () {}
-
-        public AltaCadeteViewModel (string nombre, string dir, int tel)
-        {
-            this.nombreCadete = nombre;
-            this.Direccion = dir;
-            this.Telefono = tel;
-        }
     }
 }
